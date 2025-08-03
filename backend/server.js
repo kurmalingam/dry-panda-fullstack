c5e1485a2189e55ab1ae1783d342c2df1aded9d3
Bandaru Kurmalingam <151902666+kurmalingam@users.noreply.github.com>
2025-08-03 20:16:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', authRoutes); // All auth-related routes
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log('Connected to MongoDB Atlas');
   app.listen(process.env.PORT, () =>
